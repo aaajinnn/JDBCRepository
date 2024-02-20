d9bec4b4dc264c67db25f60572cc802796df4e81
Ajin <aaajin1220@gmail.com>
2024-02-20 17:46:28 +0900

    240220 GUI - JDBC 기본 틀 구성

---
+++ jdbc/bbs/BbsDAO.java
@@ -0,0 +1,6 @@
+package jdbc.bbs;
+
+// 게시판 관련 CRUD 수행 => data layer
+public class BbsDAO {
+
+}
---
+++ jdbc/bbs/MyEventHandler.java
@@ -0,0 +1,8 @@
+package jdbc.bbs;
+
+// 이벤트 핸들러 => Application Layer
+// UI <=== Application Luyer ===> Data Layer ===> DB
+// UI와 DB를 연결해주는 다리역할
+public class MyEventHandler {
+
+}
